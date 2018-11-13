da102128cac80e7d306da892eae9401f76ab4599
FelipeAndrade <felipe.andoliv@gmail.com>
2018-11-13 09:08:42 -0200

    primeiro commit

---
+++ Frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+
+    <div>
+        <PageHeader name='Sobre' small=''></PageHeader>
+
+        <h3>React</h3>
+        <p>Curso de React, aplicação para cadastro de tarefas.</p>
+        
+    </div>
+
+)
